af5c7fe4e2189f7e1d4e732ba1f9527947681bed
Dmitriy <dudin131@mail.ru>
2022-08-11 20:41:03 +0300

    Решена седьмая задача

---
+++ task07/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите день недели");
+int n = Convert.ToInt32(Console.ReadLine());
+	if (n == 6 || n == 7)
+    {
+        Console.WriteLine("Да");    
+    }
+	else if (n < 1 || n > 7)
+    {
+        Console.WriteLine("У нас всего 7 дней в неделе!");    
+    }
+	else
+    {
+        Console.WriteLine("Нет"); 
+    } 
+		
