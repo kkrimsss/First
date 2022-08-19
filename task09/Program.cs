0b6f64aacaf6d78ffdf0275586a477216d5343b2
Dmitriy <dudin131@mail.ru>
2022-08-19 18:51:04 +0300

    Решена девятая задача

---
+++ task09/Program.cs
@@ -0,0 +1,8 @@
+﻿int x1 = 3;
+int y1 = 6;
+int z1 = 8;
+int x2 = 2;
+int y2 = 1;
+int z2 = -7;
+double result = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+Console.WriteLine(result);
