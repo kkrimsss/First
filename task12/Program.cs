02662607fc2ce1c97fed5d55893d78a951f6e588
Dmitriy <dudin131@mail.ru>
2022-08-19 18:56:57 +0300

    Решена двеннадцатая задача

---
+++ task12/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int div = 0;
+	while (i > 0){
+		div = i % 10;
+		i = i / 10;
+		sum = sum + div;
+	}
+Console.WriteLine(sum);
