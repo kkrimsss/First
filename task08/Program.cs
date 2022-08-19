78c041ae3511e8d27c4b6fc7d33c0a2a58006af6
Dmitriy <dudin131@mail.ru>
2022-08-19 18:49:12 +0300

    Решена восьмая задача

---
+++ task08/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+	if (number[0]==number[4] || number[1]==number[3]){
+	Console.WriteLine("Палиндром");
+	}
+	else{ 
+	Console.WriteLine("Не палиндром");
+	}
